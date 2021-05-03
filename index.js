7a49356872fff25d9aae84d291d0f4bace52600a
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-03 15:49:26 +0100

    add inquirer, fs, util and generateMarkdown modules

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +11,10 @@ const questions = [];
 const writeToFile = (fileName, data) => {};
 
 // TODO: Create a function to initialize app
-const init = () => {};
+const init = () => {
+  console.log("hello");
+  generateMarkdown();
+};
 
 // Function call to initialize app
 init();
